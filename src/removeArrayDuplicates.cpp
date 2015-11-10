72e68dc9e4e4d2ad4e1a5f3944b212e865094e37
PratikPatel13 <pratikpatel3395@gmail.com>
2015-11-10 21:36:50 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -15,5 +15,25 @@ NOTES: Don't create new array, try to change the input array.
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	int i, j, k;
+	if (Arr == NULL || len < 0)
+		return NULL;
+	for (i = 0; i < len; i++)
+	{
+		for (j = i + 1; j < len;)
+		{
+			if (Arr[i] == Arr[j])    // Compares each element with all the remaining elements on right side and if any duplicate found then delete by moving all other values to one position ahead.
+			{
+				for (k = j; k < len; k++)
+				{
+					Arr[k] = Arr[k + 1];
+				}
+				len--;    // decrease the length of array if the element is deleted
+			}
+			else
+			{
+				j++;  // the new element shifted to left after deletion of duplicate may be duplicate so increment only if they are not equal
+			}
+		}
+	}
 }
